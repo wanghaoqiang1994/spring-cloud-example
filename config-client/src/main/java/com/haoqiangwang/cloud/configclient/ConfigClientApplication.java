5841b9f8a1d656238e64bb341bb54edbb840af9e
wanghaoqiang1994 <w152331241709>
2020-09-01 15:16:58 +0800

    springCloud学习config配置中心

---
+++ config-client/src/main/java/com/haoqiangwang/cloud/configclient/ConfigClientApplication.java
@@ -0,0 +1,13 @@
+package com.haoqiangwang.cloud.configclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientApplication.class, args);
+    }
+
+}
---
+++ config-client/src/main/java/com/haoqiangwang/cloud/configclient/controller/ConfigController.java
@@ -0,0 +1,24 @@
+package com.haoqiangwang.cloud.configclient.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ConfigController {
+    private static final Logger logger = LoggerFactory.getLogger(ConfigController.class);
+
+    @Value("${config.name}")
+    private String name;
+
+    @Value("${config.password}")
+    private String password;
+
+    @RequestMapping(value="/config")
+    public String config(){
+        logger.info("请求读取的配置文件信息为：{}，{}",name, password);
+        return "name=" + name + ",password=" + password;
+    }
+}
